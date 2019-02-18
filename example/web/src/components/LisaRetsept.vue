db9a358348cb8d7081981b59e3b295f76d462c56
lipiir <lipiir@ttu.ee>
2019-02-18 16:15:36 +0200

    Added some views

---
+++ example/web/src/components/LisaRetsept.vue
@@ -0,0 +1,13 @@
+<template>
+    
+</template>
+
+<script>
+    export default {
+        name: "LisaRetsept"
+    }
+</script>
+
+<style scoped>
+
+</style>
---
+++ example/web/src/components/MinuKonto.vue
@@ -0,0 +1,13 @@
+<template>
+    
+</template>
+
+<script>
+    export default {
+        name: "MinuKonto"
+    }
+</script>
+
+<style scoped>
+
+</style>
