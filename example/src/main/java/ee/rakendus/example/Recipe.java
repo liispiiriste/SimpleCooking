e1ec1870f50e886ea712428f1f2727e9659eb57c
elkuus <eliskuusik@gmail.com>
2019-02-19 13:36:45 +0200

    Added get and set to db

--- example/src/main/java/ee/rakendus/example/Recipe.java
+++ example/src/main/java/ee/rakendus/example/Recipe.java
@@ -28,6 +28,54 @@ public class Recipe {
         this.portion = portion;
     }
 
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getMaterials() {
+        return materials;
+    }
+
+    public void setMaterials(String materials) {
+        this.materials = materials;
+    }
+
+    public int getPrice() {
+        return price;
+    }
+
+    public void setPrice(int price) {
+        this.price = price;
+    }
+
+    public int getPortion() {
+        return portion;
+    }
+
+    public void setPortion(int portion) {
+        this.portion = portion;
+    }
+
     @Override
     public String toString() {
         return String.format(
