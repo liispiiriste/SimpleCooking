addd5ec9879fe7f0d8053099f89f72ce36901f77
elkuus <eliskuusik@gmail.com>
2019-02-26 21:46:41 +0200

    Menu button toggles

--- example/web/src/App.vue
+++ example/web/src/App.vue
@@ -1,20 +1,14 @@
 <template>
     <div id="app" style="width:80%; margin:auto; margin-top:50px">
-        <!-- <form action="http://localhost:9000/#/MyAccount">
-            <input type="text" placeholder="Sisesta e-mail" name="email" required>
-            <input type="password" placeholder="Sisesta parool" name="password" required>
-            <button type="submit" class="logInBtn">Logi sisse</button>
-        </form>-->
         <!-- navbar -->
-        <div class="navbar navbar-expand-lg fixed-top navbar-dark bg-primary">
+        <div class="navbar navbar-expand-lg fixed-top navbar-dark bg-dark">
             <div class="container">
                 <router-link to="/" class="navbar-brand">Kodu</router-link>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor02" aria-controls="navbarColor02" aria-expanded="false" aria-label="Toggle navigation">
                     <span class="navbar-toggler-icon"></span>
                 </button>
-                <div class="collapse navbar-collapse" id="navbarResponsive">
-                    <ul class="navbar-nav">
-
+                <div class="collapse navbar-collapse" id="navbarColor02">
+                    <ul class="navbar-nav mr-auto">
                         <li class="nav-item">
                             <router-link class="nav-link" to="/recipes">Retseptid</router-link>
                         </li>
@@ -25,9 +19,14 @@
                             <router-link class="nav-link" to="/MyAccount">Minu konto</router-link>
                         </li>
                     </ul>
+                    <form class="form-inline my-2 my-lg-0">
+                        <input class="form-control mr-sm-2" type="text" placeholder="Otsing">
+                        <button class="btn btn-secondary my-2 my-sm-0" type="submit">Otsi</button>
+                    </form>
                 </div>
             </div>
-        </div><br>
+        </div>
+        <br>
         <div class="container">
             <transition name="moveInUp">
                 <router-view/>
@@ -36,16 +35,11 @@
         </div>
     </div>
 </template>
-
-
-
-
 <script>
 export default {
   name: 'app',
 }
 </script>
-
 <style>
 #app {
     font-family: 'Avenir', Helvetica, Arial, sans-serif;
@@ -55,16 +49,6 @@ export default {
     color: #2c3e50;
     margin-top: 60px;
 }
-/*ul {
-    list-style-type: none;
-}
-li{
-    width:100px;
-    float:left;
-}
-    form {
-        float:right;
-    }*/
 .moveInUp-enter-active{
     animation: fadeIn 2s ease-in;
 }
@@ -90,4 +74,7 @@ li{
         transform: translateY(-400px);
     }
 }
+    .nav-item{
+        text-align:left;
+    }
 </style>
