16f9075f847fedf8f7dbad73d898125eeba4558c
elkuus <eliskuusik@gmail.com>
2019-03-21 09:43:35 +0200

    validate registering front

--- example/web/src/components/Register.vue
+++ example/web/src/components/Register.vue
@@ -1,20 +1,23 @@
 <template>
     <div class="submitform">
 
-        <div v-if="!submitted" style="font-size:20px">
+        <div v-if="!submitted" class="register-form" style="font-size:20px">
             <h1 style="font-size:170%">Registreeri</h1>
             <div class="form-group">
                 <label for="email">Email</label>
                 <input type="text" class="form-control" id="email" required v-model="user.email" name="email">
+                <div  style="color:red;"> {{emailError}}</div>
             </div>
             <div class="form-group">
                 <label for="username">Kasutajanimi</label>
                 <input type="text" class="form-control" id="username" required v-model="user.username" name="username">
+                <div  style="color:red;"> {{usernameError}}</div>
             </div>
 
             <div class="form-group">
                 <label for="password">Parool</label>
                 <input type="text" class="form-control" id="password" required v-model="user.password" name="password">
+                <div  style="color:red;"> {{passwordError}}</div>
             </div>
 
             <button v-on:click="signUp" class="btn btn-success">Registreeri</button>
@@ -39,7 +42,10 @@
                     password:"",
                     role:"1"
                 },
-                submitted:false
+                submitted:false,
+                emailError:"",
+                usernameError:"",
+                passwordError:""
             };
         },
         methods:{
@@ -50,16 +56,35 @@
                     password: this.user.password,
                     role: this.user.role
                 };
+                if(!this.user.email){this.emailError="Lisa email" }
+                if(this.user.email){this.emailError=""}
+                if(!this.user.username){this.usernameError="Lisa kasutajanimi" }
+                if(this.user.username){this.usernameError=""}
+                if(!this.user.password){this.passwordError="Lisa parool" }
+                if(this.user.password){this.passwordError=""}
+                if(this.user.email && this.user.username && this.user.password){
                 http
                     .post("/register", data)
                     .then(response =>{
                         this.user.id = response.data.id;
                     });
-                this.submitted=true;
+                this.submitted=true;}
             }
         }
     };
 </script>
 
 <style scoped>
+    .submitform {
+        margin: auto;
+        width: 80%;
+        align: center;
+        color: #333
+    }
+    .register-form{
+        margin:auto;
+        width: 40%;
+        align:center;
+        color:#333;
+    }
 </style>
