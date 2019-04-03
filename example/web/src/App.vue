edb0b0e2412fa60234def6c3b636a80ddc6287a5
elkuus <eliskuusik@gmail.com>
2019-04-03 11:19:48 +0300

    no navbar if not logged in

--- example/web/src/App.vue
+++ example/web/src/App.vue
@@ -8,10 +8,10 @@
                     <span class="navbar-toggler-icon"></span>
                 </button>
                 <div class="collapse navbar-collapse" id="navbarColor02">
-                    <ul class="navbar-nav mr-auto">
+                    <ul v-if="authenticated" class="navbar-nav mr-auto">
 
-                        <li class="nav-item">
-                            <router-link class="nav-link" to="/recipes">Retseptid</router-link>
+                        <li  class="nav-item">
+                            <router-link  class="nav-link" to="/recipes">Retseptid</router-link>
                         </li>
                         <li class="nav-item">
                             <router-link class="nav-link" to="/add">Lisa retsept</router-link>
