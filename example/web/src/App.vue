b453c2ebccf2f58f51b54957ff1528d1bfc1de1b
elkuus <eliskuusik@gmail.com>
2019-04-17 16:09:18 +0300

    background and navbar style changes

--- example/web/src/App.vue
+++ example/web/src/App.vue
@@ -2,7 +2,32 @@
     <div id="app" style="margin:auto;">
         <div class="wrapper">
         <!-- navbar -->
-        <div class="navbar navbar-expand-lg fixed-top navbar-dark bg-dark">
+            <div>
+                <b-navbar toggleable="lg" type="dark" variant="secondary">
+                    <router-link to="/home"><b-navbar-brand>Kodu</b-navbar-brand></router-link>
+
+                    <b-navbar-toggle target="nav-collapse"></b-navbar-toggle>
+
+                    <b-collapse id="nav-collapse" is-nav>
+                        <b-navbar-nav>
+                            <b-nav-item><router-link to="/recipes"><b-nav-text >Retseptid</b-nav-text></router-link></b-nav-item>
+                            <b-nav-item><router-link to="/add"><b-nav-text>Lisa Retsept</b-nav-text></router-link></b-nav-item>
+                            <b-nav-item> <router-link to="/MyAccount"><b-nav-text>Kasutaja</b-nav-text></router-link></b-nav-item>
+                        </b-navbar-nav>
+
+                        <!-- Right aligned nav items -->
+                        <b-navbar-nav class="ml-auto">
+                            <b-nav-form>
+                                <b-form-input size="sm" class="mr-sm-2" placeholder="Search"></b-form-input>
+                                <b-button size="sm" class="my-2 my-sm-0" type="submit">Search</b-button>
+                            </b-nav-form>
+
+
+                        </b-navbar-nav>
+                    </b-collapse>
+                </b-navbar>
+            </div>
+      <!---  <div class="navbar navbar-expand-lg fixed-top navbar-dark bg-dark">
             <div class="container">
                 <router-link to="/home" class="navbar-brand">Kodu</router-link>
                 <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarColor02" aria-controls="navbarColor02" aria-expanded="false" aria-label="Toggle navigation">
@@ -25,7 +50,7 @@
                         <li class="nav-item">
                             <router-link class="nav-link" to="/register">Registreeri</router-link>
                         </li>-->
-                    </ul>
+                   <!--- </ul>
                     <form v-if="authenticated" class="form-inline my-2 my-lg-0">
                         <input class="form-control mr-sm-2" type="text" placeholder="Otsing">
                         <button class="btn btn-secondary my-2 my-sm-0" type="submit">Otsi</button>
@@ -34,7 +59,7 @@
                 </div>
 
             </div>
-        </div>
+        </div>--->
         <br>
         <div class="container" style="margin-top:45px;">
             <transition name="moveInUp">
@@ -45,8 +70,11 @@
     </div></div>
 </template>
 <script>
+
+
     import './stylesheets/main.css';
-export default {
+
+    export default {
   name: 'app',
     data() {
         return {
