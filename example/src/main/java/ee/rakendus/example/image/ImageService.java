db326079786768d6bc4b570248952649bec7756c
liispiiriste <lpiiriste@gmail.com>
2019-04-18 10:12:29 +0300

    Merge branch 'master' of C:\Users\Liis Piiriste\Documents\GitHub\SimpleCooking with conflicts.

---
+++ example/src/main/java/ee/rakendus/example/image/ImageService.java
@@ -0,0 +1,15 @@
+package ee.rakendus.example.image;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.nio.file.Path;
+
+@Service
+public class ImageService {
+    private final Path fileStorageLocation;
+
+    @Autowired
+    public ImageService(FileStoragePro)
+
+}
