a3aaf351e051caa1d0b0055fd16b6191cdef6984
Katlyyn <magikatlin@gmail.com>
2019-04-29 18:29:00 +0300

    removed some code

--- example/web/src/App.vue
+++ example/web/src/App.vue
@@ -168,28 +168,6 @@
 .search-wrapper {
     position: relative;
 }
-label {
-    position: absolute;
-    font-size: 12px;
-    color: rgba(0,0,0,.50);
-    top: 8px;
-    left: 12px;
-    z-index: -1;
-    transition: .15s all ease-in-out;
-}
-
-
-focus {
-    outline: none;
-    transform: scale(1.05);
-}
-
-
-webkit-input-placeholder {
-     font-size: 12px;
-     color: rgba(0,0,0,.50);
-     font-weight: 100;
- }
 
 
 .wrapper {
@@ -199,44 +177,8 @@ webkit-input-placeholder {
     padding-top: 12px;
 }
 
-.card {
-    box-shadow: rgba(0, 0, 0, 0.117647) 0px 1px 6px, rgba(0, 0, 0, 0.117647) 0px 1px 4px;
-    max-width: 124px;
-    margin: 12px;
-    transition: .15s all ease-in-out;
-}
-hover {
-     transform: scale(1.1);
- }
-a {
-    text-decoration: none;
-    padding: 12px;
-    color: #03A9F4;
-    font-size: 24px;
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-}
-small {
-    font-size: 10px;
-    padding: 4px;
-}
 
 
-
-.hotpink {
-    background: hotpink;
-}
-
-.green {
-    background: green;
-}
-
-.box {
-    width: 100px;
-    height: 100px;
-    border: 1px solid rgba(0,0,0,.12);
-}
 
 
 </style>
