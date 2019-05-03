51d3425f0681e10f109aa5781a5b7fec0e1b2cdf
Katlyyn <magikatlin@gmail.com>
2019-05-03 21:27:14 +0300

    removed search code from app

--- example/web/src/App.vue
+++ example/web/src/App.vue
@@ -13,6 +13,7 @@
                             <b-nav-item><router-link to="/recipes"><b-nav-text >Retseptid</b-nav-text></router-link></b-nav-item>
                             <b-nav-item><router-link to="/add"><b-nav-text>Lisa Retsept</b-nav-text></router-link></b-nav-item>
                             <b-nav-item> <router-link to="/MyAccount"><b-nav-text>Kasutaja</b-nav-text></router-link></b-nav-item>
+                            <b-nav-item> <router-link to="/search"><b-nav-text>Otsi retsepte</b-nav-text></router-link></b-nav-item>
 
                         </b-navbar-nav>
 
@@ -21,21 +22,6 @@
                                 <b-nav-text>Logi sisse</b-nav-text>
                             </router-link>
                         </b-nav-item>
-
-
-
-                        <!-- Right aligned nav items -->
-                        <b-navbar-nav class="ml-auto">
-                            <b-nav-form id="search" v-if="authenticated">
-
-                                <b-form-input size="sm" class="mr-sm-2" placeholder="Otsi retsepte..." v-model="searchStr" type="search" />
-
-                                <b-button size="sm" class="my-2 my-sm-0" to="/search" v-on:click="searchRecipe('searchStr')">
-                                    Otsi
-                                </b-button>
-                            </b-nav-form>
-
-                        </b-navbar-nav>
                     </b-collapse>
                 </b-navbar>
             </div>
@@ -55,16 +41,10 @@
     import axios from "axios";
 
     export default {
-
       name: 'app',
-
         data() {
-
-
             return {
                 authenticated: false,
-                searchStr: ''
-
             }
         },
         mounted() {
@@ -80,13 +60,6 @@
             logout() {
                 this.authenticated = false;
             },
-            searchRecipe(){
-                http.get('http://localhost:8080/api/recipes/search/' + this.searchStr)
-                    .then(response => {
-                        this.recipes = response.data;
-                    })
-
-            }
         }
 
 }
