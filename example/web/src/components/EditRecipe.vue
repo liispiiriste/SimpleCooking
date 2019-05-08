a839fe684eb8606c389f3fff75605702a3cfe925
Katlyyn <magikatlin@gmail.com>
2019-05-08 18:19:02 +0300

    name control

--- example/web/src/components/EditRecipe.vue
+++ example/web/src/components/EditRecipe.vue
@@ -5,20 +5,27 @@
             <div class="form-group">
                 <label for="edit-name">Nimi</label>
                 <input class="form-control" type="text" id="edit-name" required v-model="recipe.name"/>
-                <div class="err"> {{nameError}}</div>
+                <div class="err">
+                    {{nameError}}
+                </div>
             </div>
 
             <div class="form-group">
                 <label for="edit-description">Juhend</label>
                 <textarea class="form-control" id="edit-description" rows="3"
                           v-model="recipe.description"></textarea>
-                <div class="err"> {{desError}}</div>
+                {{this.recipe.description}}
+                <div class="err">
+                    {{desError}}
+                </div>
             </div>
 
             <div class="form-group">
                 <label for="edit-materials">Materjalid</label>
                 <textarea class="form-control" id="edit-materials" rows="3" v-model="recipe.materials"></textarea>
-                <div class="err"> {{matError}}</div>
+                <div class="err">
+                    {{matError}}
+                </div>
             </div>
 
             <div class="form-group">
@@ -29,18 +36,24 @@
                         {{category.name}}
                     </option>
                 </select>
-                <div class="err"> {{catError}}</div>
+                <div class="err">
+                    {{catError}}
+                </div>
             </div>
 
 
             <div class="form-group">
                 <label for="edit-portion">Kogus<span class="glyphicon glyphicon-euro"></span></label>
                 <input class="small-input" type="number" id="edit-portion" v-model="recipe.portion"/>
-                <div class="err"> {{portionError}}</div>
+                <div class="err">
+                    {{portionError}}
+                </div>
 
                 <label for="edit-price">Hind<span class="glyphicon glyphicon-euro"></span></label>
                 <input class="small-input" type="number" id="edit-price" v-model="recipe.price"/>
-                <div class="err"> {{priceError}}</div>
+                <div class="err">
+                    {{priceError}}
+                </div>
 
             </div>
             <div v-if="!pic">
@@ -128,8 +141,8 @@
 
                 };
                 this.nameError = (!this.recipe.name) ? "Lisa nimi" : "";
-                this.desError = (!this.recipe.description) ? "Lisa juhised" : "";
-                this.matError = (!data.materials) ? "Lisa materjalid" : "";
+                this.desError = (this.recipe.description=='') ? "Lisa juhised" : "";
+                this.matError = (!this.recipe.materials) ? "Lisa materjalid" : "";
                 this.catError = (!this.recipe.category) ? "Vali kategooria" : "";
                 this.portionError = (!this.recipe.portion) ? "Lisa portjon" : "";
                 this.priceError = (!this.recipe.price) ? "Lisa hind" : "";
@@ -227,4 +240,10 @@
         width: 20%;
         float: right;
     }
+
+    .err {
+        color: red;
+        font-size: 16px
+    }
+
 </style>
