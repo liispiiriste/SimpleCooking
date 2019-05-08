87eb059c6647e2eb2f1833cc7900a4e04fe23a0a
elkuus <eliskuusik@gmail.com>
2019-05-08 21:33:19 +0300

    recipes style changes

--- example/web/src/components/Recipes.vue
+++ example/web/src/components/Recipes.vue
@@ -1,18 +1,14 @@
 <template>
-
     <div class="recipes">
         <div class="sidemenu">
-            <b-button-group vertical>
-                <b-button v-on:click="chosenCategory('all')" active>
+            <b-button-group vertical >
+                <b-button style="opacity:0.75;" variant="info" v-on:click="chosenCategory('all')" active>
                     Kõik<input type="radio" name="options" id="all" autocomplete="off" checked></b-button>
-                <b-button v-for="(category, index) in categories" :key="index" v-on:click="chosenCategory(category.name)">
-
-                {{category.name}}<input type="radio" name="options" :id=category.name autocomplete="off" checked></b-button>
+                <b-button variant="info" style="opacity:0.75;" v-for="(category, index) in categories" :key="index" v-on:click="chosenCategory(category.name)">
+                {{category.name}}<input type="radio" name="options" :id=category.name autocomplete="off"></b-button>
             </b-button-group>
-
         </div>
-        <div class="container">
-            <div style="margin:15px; padding:10px; background:rgba(255, 255, 255, 0.2); border:none; border-radius:15px;">
+            <div class="recipe-list">
                 <h4>Retseptid</h4>
                         <ul>
                             <li v-for="(recipe, index) in recipes" :key="index"
@@ -20,26 +16,20 @@
                                 <router-link :to="{
                             name: 'recipe',
                             params: { recipe: recipe, id: recipe.id }
-                        }" style="color:#333">
+                        }">
                                     {{recipe.name}}
                                 </router-link>
                             </li>
                         </ul>
-
-
                     <div>
                         <router-view @refreshData="refreshList"></router-view>
                     </div>
-
             </div>
         </div>
-    </div>
 </template>
-
 <script>
     import http from "../http-common";
     import axios from "axios";
-
     export default {
         name: "Recipes",
         data() {
@@ -49,15 +39,12 @@
 
             }
         },
-
         methods: {
             retrieveRecipes() {
                 http.get("/recipes").then(response => {
                     this.recipes = response.data;
                 });
-
             },
-
             retrieveCategories(){
                 http.get("/categories").then(response => {
                     this.categories = response.data;
@@ -66,13 +53,10 @@
             refreshList() {
                 this.retrieveRecipes();
             },
-
-
             chosenCategory(category) {
                 if (category !== "all") {
                     http.get("/recipes/" + category).then(response => {
                         this.recipes = response.data;
-
                     })
                 } else {
                     http.get("/recipes").then(response => {
@@ -81,63 +65,13 @@
                 }
             }
         },
-
         mounted() {
             this.retrieveRecipes();
             this.retrieveCategories();
             axios.get('http://localhost:8080/api/loggedIn').then(response => (this.user = response.data));
-
         }
-
     };
 </script>
-
 <style scoped>
-
-    input {
-        visibility: hidden;
-    }
-
-    ul {
-        list-style-type: none;
-        margin: 0;
-        -webkit-columns: 2;
-        -moz-columns: 2;
-        columns: 2;
-        padding-left: 0;
-    }
-    ul li {
-        list-style-position: inside;
-        -webkit-column-break-inside: avoid;
-        page-break-inside: avoid;
-        break-inside: avoid;
-    }
-    li {
-        font: 200 17px/1.5 Helvetica, Verdana, sans-serif;
-        border-bottom: 1px solid #ccc;
-    }
-
-    li:last-child {
-        border: none;
-    }
-
-    li a {
-        text-decoration: none;
-        color: #000;
-        display: block;
-        width: auto;
-    }
-
-    li a:hover {
-        background: rgba(255, 255, 255, 0.5);
-    }
-    .container {
-        width: 55%;
-        display: block;
-        margin-left: 125px;
-    }
-    h2 {
-        margin-right: 700px;
-    }
 
 </style>
